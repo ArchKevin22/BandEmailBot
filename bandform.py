f22605c8e683971733da637ddaba8b7c4bb33953
Kevin Kou <kevin.kou24@gmail.com>
2017-11-08 10:29:04 -0800

    Added docstrings and made the UX more beginner-friendly

--- bandform.py
+++ bandform.py
@@ -27,12 +27,12 @@ import email
 import time
 from datetime import datetime
 
-#Email bot thread
 class EmailBot(threading.Thread):
+    '''Email bot thread. Where the fun stuff happens.'''
     def __init__(self, username, password):
         super(EmailBot, self).__init__()
         self._stop_event = threading.Event()
-        self.imap = imaplib.IMAP4_SSL('imap.gmail.com',993)
+        self.imap = imaplib.IMAP4_SSL('imap.gmail.com', 993)
 
         ###Your information (FILL THIS OUT CAREFULLY)###
         self.FIRSTNAME = "Kevin"
@@ -48,47 +48,51 @@ class EmailBot(threading.Thread):
 
         self.login(username, password)
     
-    #stop the email bot thread
     def stop(self):
+        '''Stop the email bot thread.
+        Closes your inbox and logs you out.'''
         self.imap.close()
         self.imap.logout()
         print("Logged out of session")
         self._stop_event.set()
 
-    #checks if the email bot has the stop flag
     def stopped(self):
+        '''Checks if the email bot has the stop flag enabled.'''
         return self._stop_event.is_set()
 
-    #attempts login for email address, program exits if failed
     def login(self, username, password):
+        '''Attempts login for email address, program exits if failed.'''
         try:
             retcode, capabilities = self.imap.login(username, password)
-            print(capabilities)
+            print(capabilities.decode("utf-8"))
             self.imap.select("Inbox")
+            print("Begin scanning...")
             self.checkEmail()
         except:
             print(sys.exc_info()[1])
             sys.exit(1)
 
-    #checks your email every X seconds (specified by the interval)
     def checkEmail(self):
-       retcode, messages = self.imap.search(None, '(UNSEEN)')
-       if retcode == 'OK':
+        '''Checks your email and hunts for the Google Form link if
+        an email from Ken Fisher or Paul Addleman is found.'''
+        retcode, messages = self.imap.search(None, '(UNSEEN)')
+        if retcode == 'OK':
             for num in messages[0].split():
-                typ, data = self.imap.fetch(num,'(RFC822)')
+                typ, data = self.imap.fetch(num, '(RFC822)')
                 msg = data[0][1].decode("utf-8").replace('\r\n', '')
-                if 'Ken Fisher' in msg:
+                if 'Ken Fisher' in msg or 'Paul Addleman' in msg:
                     print("Message found, processing %s", re.search('Subject: .* CallM', \
                     msg).group()[:-1])
-                    url = re.search('<https:\/\/docs.google.com\/forms\/.*sf_link>', msg).group()[1:-1]
-                    url = re.sub('\?.*', '', url)
-                    url = url.replace('=', '')
-                    print(url)
-                    self.submitForm(url=url)
-                    typ, data = self.imap.store(data[0].replace(' ',','),'+FLAGS','\Seen')
+                    url = re.search('<https:\/\/docs.google.com\/forms\/.*sf_link>', msg).group()
+                    if len(url) > 0:
+                        url = url[1:-1]
+                        url = re.sub('\?.*', '', url)
+                        url = url.replace('=', '')
+                        self.submitForm(url=url)
+                        typ, data = self.imap.store(data[0].replace(' ', ','), '+FLAGS', '\Seen')
 
-    #submits the form to said url
     def submitForm(self, url):
+        '''Submits the form to the given url.'''
         #initialize the structs and get the form
         starttime = datetime.now()
         url_response = re.sub('viewform.*', 'formResponse', url)
@@ -125,7 +129,7 @@ class EmailBot(threading.Thread):
         fbzx_value = re.search('[0-9-]+', fbzx_substring).group()
         fbzx_value_quotes = '"' + fbzx_value + '"'
 
-        self.submission['draftResponse'] = [None,None,fbzx_value_quotes]
+        self.submission['draftResponse'] = [None, None, fbzx_value_quotes]
         self.submission['fbzx'] = fbzx_value
 
         url_refer = re.sub('viewform.*', 'viewform\?fbzx=', url) + fbzx_value
@@ -136,22 +140,25 @@ class EmailBot(threading.Thread):
         response = requests.post(url_response, data=self.submission, headers=user_agent)
         print("Took %s seconds to submit the form" % (datetime.now() - starttime))
 
-    #loop for the email bot
     def run(self):
+        '''Event loop for the email bot.
+        While the bot isn't stopped, it will check your email every second.'''
         while True:
-            if (self.stopped()):
+            if self.stopped():
                 break
             self.checkEmail()
-            time.sleep(3)
+            time.sleep(1)
 
-#main method
 def main():
+    '''The main method.
+    Asks for your email account information and does the fun stuff.'''
     username = input("Gmail username: ")
     password = getpass()
     e = EmailBot(username, password)
     e.start()
-    str = input("Press enter at any time to stop the email bot\n")
+    input("Press ENTER or RETURN at any time to stop the email bot\n")
     e.stop()
+    print("Have a nice day!")
 
 if __name__ == '__main__':
     main()
